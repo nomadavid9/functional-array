8d988d83604921bcdf60a4913805ddf4520852d2
Peter Horton <phorton@softstackfactory.org>
2018-02-15 05:22:03 +0000

    chained the filter and map methods

--- arrayMap.js
+++ arrayMap.js
@@ -1,4 +1,4 @@
-const Animals = [
+const animals = [
     { name: "Fluffykins", species: "rabbit" },
     { name: "Caro", species: "dog" },
     { name: "Hamilton", species: "dog" },
@@ -10,23 +10,26 @@ const Animals = [
 
 //Traditional map method.
 
-function animalMap(array) {
-    let animalMapArray = [];
-    for (let i = 0; i < array.length; i++) {
-        animalMapArray.push(array[i].name);
-    }
-    return animalMapArray;
-}
+// function animalMap(array) {
+//     let animalMapArray = [];
+//     for (let i = 0; i < array.length; i++) {
+//         animalMapArray.push(array[i].name);
+//     }
+//     return animalMapArray;
+// }
 
-let animalNames = animalMap(Animals);
-console.log(animalNames);
-
-
+// let animalNames = animalMap(animals);
+// console.log(animalNames);
 
 
-// 3 lines of code for the same problem.
 
-function animalNamesMap(array) {
-    return array.map(arrayCell => arrayCell.name)
+
+//
+
+
+function animalFilterMap(array) {
+    return array.filter(arrayCell => arrayCell.species !== "cat")
+        .map(arrayCell => arrayCell.name + " is a " + arrayCell.species)
+
 };
-console.log(animalNamesMap(Animals))
+console.log(animalFilterMap(animals))
