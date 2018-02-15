9121fd45598db1b9c56ea82a3081203cb2d099cb
Peter Horton <phorton@softstackfactory.org>
2018-02-15 05:40:56 +0000

    Added a reduce method to our list of js properties, takes in two parameters (an anon function) and (initial value) the anon function takes two parameters also (accumulator) and (arrayCell)

--- arrayReduce.js
+++ arrayReduce.js
@@ -1,8 +1,22 @@
-const Animals = [
-    { name: "Fluffykins", species: "rabbit" },
-    { name: "Caro", species: "dog" },
-    { name: "Hamilton", species: "dog" },
-    { name: "Harold", species: "fish" },
-    { name: "Ursula", species: "cat" },
-    { name: "Jimmy", species: "fish" }
+const orders = [
+    { amount: 250 },
+    { amount: 400 },
+    { amount: 100 },
+    { amount: 325 }
 ]
+
+function addUp(array) {
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+        total += array[i].amount
+    }
+    return total
+}
+console.log(addUp(orders))
+
+
+function addUpFilter(array) {
+    const reducer = (accumulator, arrayCell) => accumulator + arrayCell.amount
+    return array.reduce(reducer, 0)
+}
+console.log(addUpFilter(orders))
